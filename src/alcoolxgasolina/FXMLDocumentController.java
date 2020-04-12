9d995d9f9308d6a32823c21645bc5f84857d6409
Pedro Henrique <9.pedro.deoliveira@gmail.com>
2020-04-12 10:32:57 -0300

    Gerado o dist e organização do codigo

--- src/alcoolxgasolina/FXMLDocumentController.java
+++ src/alcoolxgasolina/FXMLDocumentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package alcoolxgasolina;
 
 import java.net.URL;
@@ -23,10 +18,9 @@ public class FXMLDocumentController implements Initializable {
     @FXML    
     private Text lblResultado;
     
-    @FXML 
+    @FXML
     private TextField inputGasolinaCons, inputEtanolCons, inputEtanolPreco, inputGasolinaPreco;
     private Button btnCalc;
-    
     
     @FXML
     private void calcularRelacao(ActionEvent event) {
