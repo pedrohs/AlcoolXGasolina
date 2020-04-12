957bff36c12ca6b6c0c4e71748cd13c6fa31cdc2
Pedro Henrique <9.pedro.deoliveira@gmail.com>
2020-04-11 23:53:04 -0300

    criado interface no JavaFX Scene Builder

--- src/alcoolxgasolina/FXMLDocumentController.java
+++ src/alcoolxgasolina/FXMLDocumentController.java
@@ -22,9 +22,8 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void calcularRelacao(ActionEvent event) {
+        
     }
     
     @Override
